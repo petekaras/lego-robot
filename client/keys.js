6c09938ebe65e1114c6c3b29ecdde52e7dd531a3
pete karas <peter@peterkaras.com>
2016-03-02 11:33:16 +0000

    remove keys

--- client/keys.js
+++ client/keys.js
@@ -4,6 +4,6 @@
 */
 
 var PUBNUB_demo = PUBNUB.init({
-    publish_key: 'pub-c-a20fde3e-257d-4c7b-ac2b-6e734e0270d3',
-    subscribe_key: 'sub-c-bb03bdaa-a812-11e5-9dba-0619f8945a4f'
+    publish_key: 'pub-xxxxxxxxxxxx',
+    subscribe_key: 'sub-xxxxxxxxxx'
 });
--- server/keys.py
+++ server/keys.py
@@ -4,5 +4,5 @@
 PubNub keys
 """
 
-PUBLISH="pub-c-a20fde3e-257d-4c7b-ac2b-6e734e0270d3"
-SUBSCRIBE="sub-c-bb03bdaa-a812-11e5-9dba-0619f8945a4f"
+PUBLISH="pub-xxxxxxxxxxxxxxx"
+SUBSCRIBE="sub-xxxxxxxxxxxxx"
