1fe24ee3928c5eb7b5cfa2fa54f933485e5f8664
Peter Karas <peter@peterkaras.com>
2016-12-05 21:20:02 +0000

    making joystick controller work with chanels

--- client/messenger.js
+++ client/messenger.js
@@ -1,18 +1,21 @@
-function sendMessage(direction){
-    var doc = document.getElementById("channel");
-    var value = doc.options[doc.selectedIndex].value;
-    console.log(value);
-    if(value==='green'){
+function sendMessage(direction,channel){
+
+    console.log("Channel: " + channel, "Value: " + direction);
+
+
+    if(channel==='green'){
       green.publish({
           channel: 'dog-bot',
           message: {"move":direction}
         });
     }
-    if(value==='blue'){
+
+    if(channel==='blue'){
       blue.publish({
           channel: 'other-bot',
           message: {"move":direction}
         });
     }
+
 
   }
